d6c99fe9d0350a670555d078ce86a764c3017eef
HeZheng <742693072@qq.com>
2020-07-17 22:48:18 +0800

    Update paf_trainer.py

--- libs/trainers/paf_trainer.py
+++ libs/trainers/paf_trainer.py
@@ -15,34 +15,34 @@ class PafTrainer(object):
                  eval_iter, ckpt, device, lr_scheduler=None):
         start_time = time.time()
         log_file = os.path.join(ckpt, "log.txt")
-        log = open(log_file, "w")
+        # log = open(log_file, "w+")
         train_loader, test_loader = loader
         val_loss = 0
+        with open(log_file, 'w') as f:
+            for i, (_, imgs, targets) in enumerate(train_loader):
+                if lr_scheduler is not None:
+                    cur_iter = lr_scheduler.cur_iter + 1
+                    lr_scheduler.step(cur_iter)  # adjust learning rate
+                else:
+                    cur_iter = i + 1
+                optimizer.zero_grad()  # zeros the gradient
 
-        for i, (_, imgs, targets) in enumerate(train_loader):
-            if lr_scheduler is not None:
-                cur_iter = lr_scheduler.cur_iter + 1
-                lr_scheduler.step(cur_iter)  # adjust learning rate
-            else:
-                cur_iter = i + 1
-            optimizer.zero_grad()  # zeros the gradient
+                outputs = model(imgs.to(device))
+                loss = loss_func(outputs, targets)
 
-            outputs = model(imgs.to(device))
-            loss = loss_func(outputs, targets)
+                loss.backward()
+                optimizer.step()
+                print("LOSS: {:.8f}     ITER: {}      VAL_LOSS: {}"
+                      .format(loss.data, cur_iter, val_loss))
+                # log the loss and iteration
 
-            loss.backward()
-            optimizer.step()
-            print("LOSS: {:.8f}     ITER: {}      VAL_LOSS: {}"
-                  .format(loss.data, cur_iter, val_loss))
-            # log the loss and iteration
-
-            if cur_iter % eval_iter == 0:
-                # save the checkpoint
-                print("Computing loss on test data set ...")
-                # val_loss = self._test(model, test_loader, loss_func, device)
-                save_model(model, ckpt, optimizer, lr_scheduler)
-                log.write("LOSS: {:.8f}     ITER: {}      VAL_LOSS: {}\n"
-                          .format(loss.data, cur_iter, val_loss))
+                if cur_iter % eval_iter == 0:
+                    # save the checkpoint
+                    print("Computing loss on test data set ...")
+                    # val_loss = self._test(model, test_loader, loss_func, device)
+                    save_model(model, ckpt, optimizer, lr_scheduler)
+                f.write("LOSS: {:.8f}     ITER: {}      VAL_LOSS: {}\n"
+                      .format(loss.data, cur_iter, val_loss))
 
         end_time = time.time()
         cost_time = compute_time(start_time, end_time)
